3b65e642a0ec700fc647e8398e8b9546ba0cd8e5
PaulDelamare <paulodela2006@gmail.com>
2024-05-17 20:27:44 +0200

    ➕ Create Custom Style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,31 @@ export default {
 		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
 	],
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				condensed: ['Roboto Condensed', 'sans-serif'],
+				slab: ['Roboto Slab', 'serif'],
+				roboto: ['Roboto', 'sans-serif'],
+				akshar: ['Akshar', 'sans-serif'],
+				leckerli: ['Leckerli One', 'cursive'],
+				aoboshi: ['Aoboshi One', 'cursive']
+			},
+			colors: {
+				'light-blue': {
+					50: '#f0f9ff',
+					100: '#e0f1fe',
+					200: '#bbe4fc',
+					300: '#7fcffa',
+					400: '#3bb7f5',
+					500: '#119fe6',
+					600: '#0582ca',
+					700: '#05649f',
+					800: '#095583',
+					900: '#0d486d',
+					950: '#092d48',
+				},
+			},
+		}
 	},
 	plugins: [
 		forms,
