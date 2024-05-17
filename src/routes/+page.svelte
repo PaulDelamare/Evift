8a0bba105deae76231b75ea401ab4347711e909e
PaulDelamare <paulodela2006@gmail.com>
2024-05-17 20:29:06 +0200

    ✏️ Nothing / Test Basic Style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,14 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
-<div class="container h-full mx-auto flex justify-center items-center">
+<div class="!w-[582.5] !h-[575px] shadow-custom">
+	<h4 class="text-surface-500 inline text-shadow-custom">Pour Noël, un anniversaire ou tout autre occasion</h4>
+</div>
+
+<div class="w-full h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
+		<h1 class="h1">Evift</h1>
 		<p>Start by exploring:</p>
-		<ul>
+		<ul class="gradient">
 			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
 			<li><code class="code">/src/app.postcss</code> - app wide css</li>
 			<li>
