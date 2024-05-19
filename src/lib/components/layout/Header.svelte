e25517689bfb15e8e86dce07296449229363fce3
PaulDelamare <paulodela2006@gmail.com>
2024-05-19 19:03:55 +0200

    ✨ Create Component for Header

---
+++ src/lib/components/layout/Header.svelte
@@ -0,0 +1,22 @@
+<script>
+
+    // Imports
+	import Burger from "../extra/header/Burger.svelte";
+    import Logo from "../svg/Logo.svelte";
+	import Navigation from "./Navigation.svelte";
+
+</script>
+ 
+<!-- Bloc for Header -->
+<div class="wrap py-2 px-8 between items-end relative">
+
+    <!-- Display Logo -->
+    <Logo gradient text link h1/>
+
+    <!-- TODO Add condition for Loged Nav -->
+    <!-- For Desktop Display Navigation -->
+    <Navigation navClass="tablet:hidden" ulClass="gap-12" hover/>
+
+    <!-- For Responsive Display Burger Menu -->
+    <Burger divClass="hidden tablet:block" />
+</div>
