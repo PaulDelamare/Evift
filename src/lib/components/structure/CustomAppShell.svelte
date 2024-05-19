ab282d1792109c57588d872325ed3010097c8d1a
PaulDelamare <paulodela2006@gmail.com>
2024-05-19 19:11:54 +0200

    ✨ Create Skeleton App Shell

---
+++ src/lib/components/structure/CustomAppShell.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	// Import parameters
+	// Class
+	export let headerClass: string = '';
+	export let mainClass: string = '';
+	export let footerClass: string = '';
+
+	// Position
+	// Sticky
+	export let headerSticky: boolean = false;
+
+	// Fixed
+	export let headerFixe: boolean = false;
+	export let footerFixe: boolean = false;
+
+	// Position class
+	const sticky = 'sticky top-0 z-50';
+
+	// Create class
+	const headerC = `${headerClass} ${headerSticky ? sticky : ''} ${headerFixe ? 'fixed top-0 w-full' : ''} bg-surface-500`;
+	const mainC = `flex-auto ${mainClass}`;
+	const footerC = `${footerClass} ${footerFixe ? 'fixed bottom-0 w-full' : ''}`;
+</script>
+
+<!-- APP SHELL -->
+<div class="relative w-full h-full flex flex-col overflow-hidden" data-testid="app-shell">
+	<div class="flex-auto w-full h-full flex overflow-hidden">
+		<div
+			id="page"
+			class="relative flex-1 overflow-x-hidden flex flex-col"
+			style="scrollbar-gutter: auto;"
+		>
+			<!-- Elements -->
+			<!-- Header -->
+			<header class={headerC}>
+				<slot name="header" />
+			</header>
+
+			<!-- Main (Content) -->
+			<main class={mainC}>
+				<slot />
+			</main>
+
+			<!-- Footer -->
+			<footer class={footerC}>
+				<slot name="footer" />
+			</footer>
+		</div>
+	</div>
+</div>
