bee5aa09fc297a00f16d1d8b57171de4d3e66216
PaulDelamare <paulodela2006@gmail.com>
2024-05-24 18:05:58 +0200

    ✨ Create Hera and Responsive

---
+++ src/lib/components/home/Hero.svelte
@@ -0,0 +1,32 @@
+<section class="gradient-secondary h-[50rem] max-h-[94svh]">
+    <div class="wrap h-full between px-12 gap-8 tablet:flex-col tablet:py-12 tablet:px-4 tablet:justify-evenly">
+        <div class="column w-2/5 mini-desk:w-3/5 text-center gap-8 text-surface-500 tablet:w-full ">
+            <h2 class="text-shadow-custom">Créez votre événement</h2>
+            <h4 class="text-shadow-custom tablet:text-xl">Pour Noël, un anniversaire ou tout autre occasion</h4>
+            <a href="/connexion" class="nav px-12 py-2 bg-secondary-500 rounded-xl shadow-custom">
+                Je commence
+            </a>
+        </div>
+        <div class="w-[719px] mini-desk:max-w-[400px] tablet:!w-full mockup">
+            <img class="w-full" src="/mockup/mockup-evift.png" alt="Mockup du site EVIFT sur appareil mobile et tablette, montrant la page d'accueil avec un événement en vedette et une liste des événements à venir" />
+        </div>
+    </div>
+</section>
+
+<style>
+    .mockup {
+		animation: bounce 7s infinite ease;
+	}
+    @keyframes bounce {
+		0% {
+			transform: translateY(0);
+		}
+		50% {
+			transform: translateY(-30px) rotate(2deg);
+		}
+		100% {
+			transform: translateY(0);
+		}
+	}
+
+</style>
