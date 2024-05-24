062bdcef5c8c4dbe8fa0b737490157faad188af0
PaulDelamare <paulodela2006@gmail.com>
2024-05-25 00:54:33 +0200

    ➕ Implement Type Writter

--- src/lib/components/home/Hero.svelte
+++ src/lib/components/home/Hero.svelte
@@ -1,8 +1,36 @@
+<script>
+    import Typewriter from 'svelte-typewriter'
+
+    let text = 0;
+    setInterval(() => {
+        text ++;
+        if(text >= 3) text = 0
+    }, 12000);
+
+</script>
+  
+  
 <section class="bg-secondaryGradient h-[50rem] max-h-[94svh]">
     <div class="wrap h-full between px-12 gap-8 tablet:flex-col tablet:py-12 tablet:px-4 tablet:justify-evenly">
         <div class="column w-2/5 mini-desk:w-3/5 text-center gap-8 text-surface-500 tablet:w-full ">
-            <h2 class="text-shadow-custom">Créez votre événement</h2>
-            <h4 class="text-shadow-custom tablet:text-xl">Pour Noël, un anniversaire ou tout autre occasion</h4>
+            <Typewriter interval={60} cursor = {false} unwriteInterval = {0}> 
+                {#if text == 0}
+                    <h2 class="text-shadow-custom">Créez votre événement</h2>
+                {:else if text == 1}
+                    <h2 class="text-shadow-custom">Rejoingnez un événement</h2>
+                {:else if text == 2}
+                    <h2 class="text-shadow-custom">Créez votre liste de cadeaux</h2>
+                {/if}
+            </Typewriter>
+            <Typewriter interval={30} cursor = {false} unwriteInterval = {0}>
+                {#if text == 0}
+                    <h4 class="text-shadow-custom tablet:text-xl">Pour Noël, un anniversaire ou tout autre occasion !</h4>
+                {:else if text == 1}
+                    <h4 class="text-shadow-custom tablet:text-xl">Pour y participer et consulter l'organisation super simplement !</h4>
+                {:else if text == 2}
+                    <h4 class="text-shadow-custom tablet:text-xl">Pour que vos proches puissent vous rendre heureux !</h4>
+                {/if}
+            </Typewriter>
             <a href="/connexion" class="nav px-12 py-2 bg-secondary-500 rounded-xl shadow-custom">
                 Je commence
             </a>
