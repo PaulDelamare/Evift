53e6384a7452f6918af3c7e6f54f96d71436c03e
PaulDelamare <paulodela2006@gmail.com>
2024-05-24 21:17:40 +0200

    ✨ Create page Layout

---
+++ src/lib/components/structure/PageLayout.svelte
@@ -0,0 +1,7 @@
+<!-- Display Page Hero -->
+<slot name="hero" />
+<!-- Place div whith flex display -->
+<div class="flex flex-col gap-24 py-24 mini-tablet:gap-12 mini-tablet:py-12">
+    <!-- Display all page content -->
+    <slot />
+</div>
