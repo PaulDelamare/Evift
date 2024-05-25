55f06343979edbb6a27fbb1086fd59c6fdbc0656
PaulDelamare <paulodela2006@gmail.com>
2024-05-25 11:05:46 +0200

    ➕ Create Background style

--- src/lib/components/structure/PageLayout.svelte
+++ src/lib/components/structure/PageLayout.svelte
@@ -1,7 +1,30 @@
+<script>
+    // Imports
+    export let pinkBackground = true;
+    export let pinkWidth = 'w-[526px] mini-tablet:w-[400px]';
+    export let pinkTop = 'top-1/4 mini-tablet:top-52';
+    export let pinkLeft = '-left-10';
+
+    // Imports
+    export let blueBackground = true;
+    export let blueWidth = 'w-[872px] mini-tablet:w-[600px]';
+    export let blueBottom = '-bottom-1/4 mini-tablet:bottom-52';
+    export let blueRight = 'right-0 mini-tablet:-right-52';
+</script>
 <!-- Display Page Hero -->
 <slot name="hero" />
 <!-- Place div whith flex display -->
-<div class="flex flex-col gap-24 py-24 mini-tablet:gap-12 mini-tablet:py-12">
+<div class="flex flex-col gap-24 py-24 mini-tablet:gap-12 mini-tablet:py-12 relative ">
+    {#if pinkBackground}
+        <div class="{pinkWidth} {pinkTop} {pinkLeft} absolute -z-10">
+            <img class="w-full" src="/svg/background/pinkBackground.svg" alt="Style de Fond du site EVIFT" />
+        </div>
+    {/if}
+    {#if blueBackground}
+        <div class="{blueWidth} {blueBottom} {blueRight} absolute -z-10">
+            <img class="w-full" src="/svg/background/blueBackground.svg" alt="Style de Fond du site EVIFT" />
+        </div>
+    {/if}
     <!-- Display all page content -->
     <slot />
 </div>
