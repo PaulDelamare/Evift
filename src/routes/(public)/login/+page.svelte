041425014a8b08944bb71b9d40f480a93d28ce39
Paul_Delamare <paulodela2006@gmail.com>
2024-05-28 10:57:25 +0200

    ✨ Create new page for Login/Register Page

---
+++ src/routes/(public)/login/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import Login from '$lib/components/public/login/Login.svelte';
+import PageLayout from '$lib/components/structure/PageLayout.svelte';
+    
+</script>
+
+<PageLayout>
+    <!-- Section with wrap for Display Content -->
+    <section class="wrap px-8">
+        <!-- Bodrer radius and Overflow hidden for have rounded in the two components -->
+        <div class="rounded-xl overflow-hidden">
+            <Login />
+        </div>
+    </section>
+</PageLayout>
