902fc1f77c399e97d54cb661b340fd429ce3af52
Paul_Delamare <paulodela2006@gmail.com>
2024-06-02 19:51:43 +0200

    ➕ Add Informtion for page.svelte

--- src/routes/(public)/login/+page.svelte
+++ src/routes/(public)/login/+page.svelte
@@ -1,15 +1,83 @@
 <script lang="ts">
-    import Login from '$lib/components/public/login/Login.svelte';
+    // Import components
+	import Layer from '$lib/components/public/loginRegister/layer/Layer.svelte';
+    import Login from '$lib/components/public/loginRegister/login/Login.svelte';
+    import Register from '$lib/components/public/loginRegister/register/Register.svelte';
     import PageLayout from '$lib/components/structure/PageLayout.svelte';
-    
+
+    // Variables
+    // Login variable for know if user is Activated
+    let loginActivated = true;
+    // Email variable
+    let email = '';
+
+    // Screen width
+    let innerWidth = 0;
+
+	// Functions
+	/**
+	 * Change loginActivated to the opposite.
+	 *
+	 * This function is used for alternate between login and register
+	 *
+	 * @return void This function returns nothing.
+	 */
+    const handleActivate = () => {
+        loginActivated = !loginActivated;
+        if (innerWidth < 850) {
+            const element: HTMLElement | null = document.querySelector('#top');
+            if (element) {
+                scrollToTop(element);
+            }
+        }
+    }
+
+	/**
+	 * Change email variable to the new value.
+	 *
+	 * This function is used for have the same value in login and register input if user do the wrong form
+	 *
+	 * @return void This function returns nothing.
+	 */
+    const  handleEmailChange = (event: Event) => {
+        if (event.target instanceof HTMLInputElement) {
+            email = event.target.value;
+        }
+    }
+
+    /**
+     * Scroll to the top of the page.
+     *
+     * @param HTMLElement node - The element to scroll to.
+     * @return void This function does not return anything.
+     */
+    const scrollToTop = (node: HTMLElement) => {
+        console.log(node.id)
+        if (node) {
+        // Scroll to the element with smooth behavior.
+            node.scrollIntoView({ behavior: 'smooth' });
+        }
+    }
+
 </script>
 
-<PageLayout>
-    <!-- Section with wrap for Display Content -->
-    <section class="wrap px-8">
-        <!-- Bodrer radius and Overflow hidden for have rounded in the two components -->
-        <div class="rounded-xl overflow-hidden">
-            <Login />
+<!-- For know the screen width -->
+<svelte:window bind:innerWidth />
+
+<!-- Page Layout for Display Element -->
+<PageLayout padding="py-12">
+    <!-- Wrap container -->
+    <section class="wrap px-8 mobile-large:px-4">
+        <!-- Login Register comoponen -->
+        <div class="tablet:overflow-visible tablet:shadow-none tablet:gap-12 tablet:!flex-col rounded-xl flex overflow-hidden shadow-[41px_41px_82px_0_rgba(190,190,190,1),-41px_-41px_82px_0_rgba(255,255,255,1)] custom-transition min-h-[667px] relative {!loginActivated ? 'justify-end': ''}">
+            <!-- Add Login and Register component -->
+            <!-- Login with Form -->
+            <Login {loginActivated} {email} {handleEmailChange}/>
+            <!-- Register with Form -->
+            <Register {loginActivated} {email} {handleEmailChange} />
+
+            <!-- Add Layer component for change into login or register -->
+            <Layer {loginActivated}  {handleActivate}/>
         </div>
     </section>
 </PageLayout>
