96956a0b360c5da58dd68a08bdc461547671aed0
Paul_Delamare <paulodela2006@gmail.com>
2024-06-02 19:50:28 +0200

    ✨ Create Model for Global Form Login and Register

---
+++ src/lib/models/form/formLoginRegister.model.ts
@@ -0,0 +1,14 @@
+interface Input {
+    type: 'password' | 'text',
+    label: string,
+    name: string,
+    value: string,
+    onInput?: (event: Event) => void
+}
+
+interface GroupInput {
+    type: 'group',
+    inputs: Input[]
+}
+
+export type FormInput = Input | GroupInput
