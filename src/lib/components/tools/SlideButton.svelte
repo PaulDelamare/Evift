f49ed54f6d1d427d704ad76a4fd6100d12e03edc
Paul_Delamare <paulodela2006@gmail.com>
2024-06-04 20:08:22 +0200

    ✏️ Add Comment

--- src/lib/components/tools/SlideButton.svelte
+++ src/lib/components/tools/SlideButton.svelte
@@ -1,23 +1,33 @@
 <script lang="ts">
+	// Import components
 	import Arrow from '../svg/Arrow.svelte';
 
+	// Import variable
+	// Function when button is clicked
 	export let handleActivate = (): void => {};
+	// Text in Button
 	export let text: string;
+	// Way to display
 	export let toRight = true;
+
 </script>
 
+<!-- Button with animation -->
 <button
 	on:click={handleActivate}
 	class="{toRight
 		? 'pr-[1.2em] pl-[3.3em]'
 		: 'pl-[1.2em] pr-[3.3em]'} cssbuttons-io-button group nav uppercase bg-transparent text-surface-500 p-[0.35em] text-base font-medium rounded-full border-none tracking-widest flex items-center overflow-hidden relative h-[2.8em] cursor-pointer outline outline-2"
 >
+<!-- Display the text -->
 	{text}
+	<!-- Bloc for Animation -->
 	<div
 		class="{toRight
 			? ' left-[0.3em] '
-			: ' right-[0.3em]'} icon group-active:scale-95 bg-surface-500 absolute flex items-center justify-center h-[2.2em] w-[2.2em] rounded-full transition-all duration-300"
+			: ' right-[0.3em]'} group-hover:w-[calc(100%-0.6em)] icon group-active:scale-95 bg-surface-500 absolute flex items-center justify-center h-[2.2em] w-[2.2em] rounded-full transition-all duration-300"
 	>
+	<!-- Display the arrow -->
 		<Arrow
 			classSvg="{toRight
 				? ''
@@ -25,9 +35,3 @@
 		/>
 	</div>
 </button>
-
-<style lang="postcss">
-	.cssbuttons-io-button:hover .icon {
-		@apply w-[calc(100%-0.6em)];
-	}
-</style>
