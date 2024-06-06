41ae9e34367025ef165edc0f8940188a7b9374a1
PaulDelamare <paulodela2006@gmail.com>
2024-06-06 23:04:59 +0200

    ✨ Create Component for bg color

---
+++ src/lib/components/structure/BlueBg.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+
+    export let blueWidth: string;
+    export let blueBottom: string;
+    export let blueRight: string ;
+
+</script>
+
+<div class="{blueWidth} {blueBottom} {blueRight} absolute -z-10">
+    <img
+        class="w-full"
+        src="/svg/background/blueBackground.svg"
+        alt="Style de Fond du site EVIFT"
+    />
+</div>
