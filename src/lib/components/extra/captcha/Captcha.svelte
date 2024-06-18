fd9c7a466547781fc9a7160cd94fb6f2439b3efc
Paul_Delamare <paulodela2006@gmail.com>
2024-06-18 11:17:09 +0200

    ✏️ Add comment

--- src/lib/components/extra/captcha/Captcha.svelte
+++ src/lib/components/extra/captcha/Captcha.svelte
@@ -1,37 +1,55 @@
 <script lang="ts">
-	import { PUBLIC_CAPTCHA_KEY } from "$env/static/public";
-	import { onMount } from "svelte";
+	// ! IMPORTS
+	import { PUBLIC_CAPTCHA_KEY } from '$env/static/public';
+	import { onMount } from 'svelte';
 
-     let State = {
+	// ! VARIABLE
+	// 
+	export let token: string;
+
+	// Define state captcha
+	let State = {
 		idle: 'idle',
 		requesting: 'requesting',
 		success: 'success'
 	};
 
-     export let token: string;
+	// Define state
 	let state = State.idle;
 
+	// Function execute on submit
 	export function onSubmitCaptcha() {
+		// Change state
 		state = State.requesting;
+
+		// Execute do recaptcha for get the token
 		doRecaptcha();
 	}
 
+	// Get toke function
 	function doRecaptcha() {
-          // @ts-ignore
+		// @ts-ignore
 		grecaptcha.ready(function () {
-               // @ts-ignore
+			// @ts-ignore
 			grecaptcha.execute(PUBLIC_CAPTCHA_KEY, { action: 'submit' }).then(function (t) {
 				state = State.success;
-				token = t;				
+				token = t;
 			});
 		});
 	}
-	onMount(() => {
-		onSubmitCaptcha();
-	})
 
+	// Mount
+	onMount(() => {
+		// Execute
+		onSubmitCaptcha();
+	});
 </script>
 
+<!-- Define in header the captcha -->
 <svelte:head>
-	<script src="https://www.google.com/recaptcha/api.js?render={PUBLIC_CAPTCHA_KEY}" async defer></script>
+	<script
+		src="https://www.google.com/recaptcha/api.js?render={PUBLIC_CAPTCHA_KEY}"
+		async
+		defer
+	></script>
 </svelte:head>
