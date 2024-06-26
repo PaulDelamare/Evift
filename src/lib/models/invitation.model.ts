42b9e7f9cab1d1b83f77fbe0462bf1e2fb1244d4
PaulDelamare <paulodela2006@gmail.com>
2024-06-26 23:27:32 +0200

    ➕ Add Component for Invitation data

--- src/lib/models/invitation.model.ts
+++ src/lib/models/invitation.model.ts
@@ -2,3 +2,21 @@ export interface GetCountFriendInvitation {
     data: number
     status: number;
 }
+
+export interface InvitationData{
+    data: Invitation[];
+    status: number;
+}
+
+export interface Invitation {
+    id: string;
+    userId: string;
+    requestId: string;
+    createdAt: Date
+    user: {
+        id: string;
+        email: string;
+        firstname: string;
+        lastname: string;
+    }
+}
