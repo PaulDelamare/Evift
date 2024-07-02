a92675daa16c9209638e8f6fedada8fc4cf3962c
PaulDelamare <paulodela2006@gmail.com>
2024-07-02 13:54:26 +0200

    ➕ Add new modal

--- src/lib/components/modal/allModal.ts
+++ src/lib/components/modal/allModal.ts
@@ -1,10 +1,17 @@
 import type { ModalComponent } from '@skeletonlabs/skeleton';
 import ContactModal from './ContactModal.svelte';
+import FindUserByEmail from './FindUserByEmail.svelte';
 
 export const modalComponentRegistry: Record<string, ModalComponent> = {
 	ContactModal: {
 		// Pass a reference to your custom component
 		ref: ContactModal
+		// Add the component properties as key/value pairs
+		// props: { },
+	},
+	FindUserByEmail: {
+		// Pass a reference to your custom component
+		ref: FindUserByEmail
 		// Add the component properties as key/value pairs
 		// props: { },
 	}
