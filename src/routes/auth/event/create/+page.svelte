56a820d54124387b7ac04fd9b4743aed35139c94
PaulDelamare <paulodela2006@gmail.com>
2024-07-05 11:47:40 +0200

    ➕ Add Create event page

---
+++ src/routes/auth/event/create/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import EventForm from '$lib/components/auth/form/EventForm.svelte';
+	import PageLayout from '$lib/components/structure/PageLayout.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<PageLayout gap="gap-8" padding="py-8">
+	<div class="between wrap px-4">
+		<h2 class="mini-tablet:text-center text-gradient">Créez votre événement</h2>
+	</div>
+	<EventForm action="?/createEvent" />
+</PageLayout>
