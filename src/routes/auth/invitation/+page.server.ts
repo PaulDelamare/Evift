c65d2e48cef482bf6cfe44cf740ba90d4319f079
PaulDelamare <paulodela2006@gmail.com>
2024-07-05 11:48:06 +0200

    ➕ Add method for accept refuse event

--- src/routes/auth/invitation/+page.server.ts
+++ src/routes/auth/invitation/+page.server.ts
@@ -4,16 +4,22 @@ import type { Actions } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import * as yup from 'yup';
 import { convertStringToBoolean } from '$lib/functions/utils/transformStringToBoolean';
+import { API_URL } from '$env/static/private';
 
 export const load = (async ({ fetch }) => {
 	// Instance Invitation Api
 	const notifApi = new InvitationApi(fetch);
 	// Get Notification number
 	const invitations = await notifApi.getInvitations();
+	const invitationsEvent = await notifApi.getEventInvitation();
+
+	const imgUrl = API_URL
 
 	// Return data
 	return {
-		invitations: invitations.data
+		invitations: invitations.data,
+		invitationsEvent: invitationsEvent.data,
+		imgUrl
 	};
 }) satisfies PageServerLoad;
 
@@ -89,9 +95,68 @@ export const actions: Actions = {
 		}
 		// Else return success
 		return {
-			success: true,
+			successFriend: true,
 			idRemove: invitationId,
-			accept: accept
+			acceptFriend: accept
+		};
+	},
+
+	acceptOrRefuseEvent: async ({ request, fetch }) => {
+		// Get form data
+		const data = await request.formData();
+		const invitationId = data.get('eventId') as string;
+		let accept = data.get('accept') as string | boolean;
+		const errors: Errors = {};
+
+		console.log(invitationId, accept);
+
+		// ? Validation
+		try {
+			// Validation schema
+			await schema.validate({ invitationId, accept }, { abortEarly: false });
+		} catch (error) {
+			// - Catch Errors
+			// If Error in ValidationError
+			if (error instanceof yup.ValidationError) {
+				// Check what error it is and return this in errors instance
+				error.inner.forEach((err) => {
+					if (err.path === 'invitationId') {
+						errors.error = err.message;
+					}
+
+					if (err.path === 'accept') {
+						errors.error = err.message;
+					}
+				});
+
+				// Return errors
+				return { status: 400, errors };
+			} else {
+				// Else Throw custom Error
+				return { status: 400, errors: 'Une erreur est survenue' };
+			}
+		}
+
+		// Convert string to boolean
+		accept = convertStringToBoolean(accept as string);
+
+		// Instance Invitation in api
+		const api = new InvitationApi(fetch);
+		// Call responseInvitation
+		const res = await api.responseEventInvitation(invitationId, accept);
+		console.log(res)
+
+		// if an error occurs
+		if ('error' in res) {
+			// Return error
+			errors.error = res?.error;
+			return { status: 400, errors };
+		}
+		// Else return success
+		return {
+			successEvent: true,
+			acceptEvent: accept,
+			idRemoveEvent : invitationId
 		};
 	}
 };
