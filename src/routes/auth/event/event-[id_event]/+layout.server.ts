28bd4342310eb7c5ce741659e63ade8494557a6d
PaulDelamare <paulodela2006@gmail.com>
2024-07-06 22:56:55 +0200

    ✨ Create details event

---
+++ src/routes/auth/event/event-[id_event]/+layout.server.ts
@@ -0,0 +1,23 @@
+import EventApi from '$lib/server/event.server';
+import { error } from '@sveltejs/kit';
+import type { LayoutServerLoad } from './$types';
+import { API_URL } from '$env/static/private';
+
+export const load = (async ({params, fetch}) => {
+    const id_event = params.id_event;
+
+    const api = new EventApi(fetch);
+    const event = await api.getOneEvent(id_event);
+
+    const imgUrl = API_URL;
+
+    if (event.status !== 200) {
+        console.log(event)
+        throw error(401, 'Vous n\'êtes pas autorisé à accéder à cet événement');
+    }
+
+    return {
+        event: event.data,
+        imgUrl
+    }
+}) satisfies LayoutServerLoad;
