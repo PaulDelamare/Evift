033fadbdc191da40ec4c163680f4abdc544c8444
PaulDelamare <paulodela2006@gmail.com>
2024-07-11 00:39:31 +0200

    ➕ Add responsive and add link

--- src/routes/auth/event/event-[id_event]/+page.svelte
+++ src/routes/auth/event/event-[id_event]/+page.svelte
@@ -17,8 +17,8 @@
 
 <PageLayout>
 	<section slot="hero" class="bg-gradient py-12">
-		<div class="wrap flex flex-col">
-			<div class=" flex items-center justify-center gap-8 text-surface-500">
+		<div class="wrap px-4 flex flex-col gap-4">
+			<div class=" flex items-center justify-center gap-8 text-surface-500 mini-tablet:flex-col">
 				<div class="h-44 tablet:w-full w-80 overflow-hidden box-border shadow-md rounded-3xl">
 					{#if event.img}
 						<img
@@ -32,17 +32,17 @@
 						</div>
 					{/if}
 				</div>
-				<div class="min-w-80 flex flex-col gap-2">
-					<address class="text-surface-500">{event.address}</address>
+				<div class="min-w-80 flex flex-col gap-2 mini-tablet:text-center px-4">
+					<address class="text-surface-500 ">{event.address}</address>
 					<time class="text-surface-500">{event.time}</time>
 					<h3>{event.name}</h3>
-					<p>{event.description}</p>
+					<p class="break-all max-w-[280px]">{event.description}</p>
 				</div>
 			</div>
 			<button
 				on:click={() =>
 					renderModal(modalStore, 'ParticipantModel', 'Participants', 'Participants')}
-				class="bg-surface-500 self-end rounded-lg flex items-center gap-4 py-2 px-3 text-secondary-500 uppercase font-bold hover:scale-[1.02] custom-transition active:scale-90"
+				class="bg-surface-500 mini-tablet:self-center self-end rounded-lg flex items-center gap-4 py-2 px-3 text-secondary-500 uppercase font-bold hover:scale-[1.02] custom-transition active:scale-90"
 			>
 				<div class="w-4">
 					<UserSvg classSvg="fill-secondary-500 w-6" />
@@ -53,7 +53,7 @@
 		</div>
 	</section>
 	<section>
-		<div class=" wrap gap-8 grid grid-cols-[repeat(auto-fit,_minmax(360px,_1fr))] px-4">
+		<div class=" wrap gap-8 grid grid-cols-[repeat(auto-fit,_minmax(360px,_1fr))] mini-tablet:grid-cols-[repeat(auto-fit,_minmax(280px,_1fr))] px-4">
 			<div
 				class="w-full min-h-96 bg-gradient rounded-3xl flex flex-col justify-center items-center gap-8 text-surface-500"
 			>
@@ -61,7 +61,7 @@
 				<LogoSvg fill="fill-surface-500" widthSvg={75} />
 				<!-- </div> -->
 				<h3>Liste de cadeaux</h3>
-				<a href="/">
+				<a href="/auth/event/event-{id_event}/gifts">
 					<SlideButton text="Accéder" toRight={false} />
 				</a>
 			</div>
