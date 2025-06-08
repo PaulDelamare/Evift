0a76b8e680762ef65a869fd226ebd2d43cf3d935
PaulDelamare <paulodela2006@gmail.com>
2025-06-08 12:47:38 +0200

    ♻️ Simplifie la classe UserApi en supprimant le type générique et des commentaires inutiles

--- src/lib/server/user.server.ts
+++ src/lib/server/user.server.ts
@@ -1,12 +1,10 @@
 // ! IMPORTS
 import { env } from '$env/dynamic/private';
 import { Api } from './api.server';
-import type { GetCountFriendInvitation } from '$lib/models/invitation.model';
 import type { DataUser, User } from '$lib/models/user.model';
 
 // ! Class
-export default class UserApi extends Api<GetCountFriendInvitation> {
-	// Base url request for auth methods
+export default class UserApi extends Api {
 	private authUrl = `${env.API_URL}api/user/`;
 
 	/**
@@ -16,9 +14,7 @@ export default class UserApi extends Api<GetCountFriendInvitation> {
 	 * @throws {Error} If there is an error retrieving the count of friend invitations.
 	 */
 	getUserByEmail = async (email: string): Promise<User> => {
-		// - Try Validation
 		try {
-			// Do request
 			const response = await this.fetch(`${this.authUrl}findUser/${email}`, {
 				method: 'GET',
 				headers: {
@@ -27,13 +23,11 @@ export default class UserApi extends Api<GetCountFriendInvitation> {
 				credentials: 'include'
 			});
 
-			// Get Count Invitation
 			const data: DataUser = await response.json();
-
-			// Return data
 			return data.data;
+
 		} catch (error) {
-			// - Catch Errors
+
 			throw new Error('Error Get Count : ' + error);
 		}
 	};
