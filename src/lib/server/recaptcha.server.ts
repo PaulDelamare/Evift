9b34b59d7582c4ad9d627ce4c3a2235cb3a4133d
PaulDelamare <paulodela2006@gmail.com>
2025-06-08 12:47:33 +0200

    ♻️ Simplifie la classe CaptchaApi en supprimant le type générique et des commentaires inutiles

--- src/lib/server/recaptcha.server.ts
+++ src/lib/server/recaptcha.server.ts
@@ -3,14 +3,8 @@ import { env } from '$env/dynamic/private';
 import { Api } from './api.server';
 
 // ! Class
-export default class CaptchaApi extends Api<{
-	success: boolean;
-	challenge_ts: string;
-	hostname: string;
-	score: 0.9;
-	action: string;
-}> {
-	// - Create recapctha request validator
+export default class CaptchaApi extends Api {
+
 	captcha = async (
 		captcha: string
 	): Promise<{
@@ -20,11 +14,9 @@ export default class CaptchaApi extends Api<{
 		score: 0.9;
 		action: string;
 	}> => {
-		// ? Try request
+
 		try {
 
-			// Define request
-			// Pass recapctah validator key and captcha token
 			const response = await this.fetch(
 				`https://www.google.com/recaptcha/api/siteverify?secret=${env.SERVER_CAPTCHA_KEY}&response=${captcha}`,
 				{
@@ -32,12 +24,10 @@ export default class CaptchaApi extends Api<{
 					headers: {
 						'Content-Type': 'application/json'
 					},
-					// Need to include credentials for have x-api-key in request with hooks
 					credentials: 'include'
 				}
 			);
 
-			// Get data response
 			const data: {
 				success: boolean;
 				challenge_ts: string;
@@ -46,11 +36,10 @@ export default class CaptchaApi extends Api<{
 				action: string;
 			} = await response.json();
 
-			// Return data
 			return data;
+
 		} catch (error) {
-			// ? Catch Request
-			// Throw error
+
 			throw new Error('Error captcha : ' + error);
 		}
 	};
