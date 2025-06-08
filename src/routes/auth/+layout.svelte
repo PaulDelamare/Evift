526a57ae780a367819c993b1c967abc0459bea81
PaulDelamare <paulodela2006@gmail.com>
2025-06-08 12:47:41 +0200

    ♻️ Ajoute le fichier +layout.svelte pour gérer l'authentification avec le composant Driver

---
+++ src/routes/auth/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { LayoutData } from './$types';
+	import Driver from '$lib/driver/Driver.svelte';
+
+	export let data: LayoutData;
+
+	const user = data.user;
+
+	const firstLogin = user.firstLogin;
+</script>
+
+{#if firstLogin}
+	<Driver />
+{/if}
+
+<slot />
