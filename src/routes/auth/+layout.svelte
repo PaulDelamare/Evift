4eb0dcc8dc6754507098a6a6f569fc858db4e775
Paul_Delamare <paulodela2006@gmail.com>
2025-06-27 16:15:48 +0200

    🔧 Ajoute le type ActionData dans le script et exporte le formulaire pour la gestion des actions

--- src/routes/auth/+layout.svelte
+++ src/routes/auth/+layout.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
-	import type { LayoutData } from './$types';
+	import type { ActionData, LayoutData } from './$types';
 	import Driver from '$lib/driver/Driver.svelte';
+	import { invalidateAll } from '$app/navigation';
 
 	export let data: LayoutData;
 
 	let user = data.user;
 
 	const firstLogin = user.firstLogin;
+
+	export let form: ActionData;
+
+
 </script>
 
 {#if firstLogin}
