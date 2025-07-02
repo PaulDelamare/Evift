64283637f63cf997a8368003f97e97625de660cf
PaulDelamare <paulodela2006@gmail.com>
2025-07-02 12:25:04 +0200

    🔧 Améliore la gestion des erreurs dans la méthode load et les actions findUserByEmail et sendFriendsInvitation en utilisant executeOrThrow et en accédant correctement à res.error.error.

--- src/routes/auth/friends/+page.server.ts
+++ src/routes/auth/friends/+page.server.ts
@@ -1,77 +1,64 @@
-// ! IMPORTS
 import FriendsApi from '$lib/server/friends.server';
-import { error, redirect, type Actions } from '@sveltejs/kit';
+import type { Actions } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import * as yup from 'yup';
 import UserApi from '$lib/server/user.server';
 import InvitationApi from '$lib/server/invitation.server';
+import { executeOrThrow } from '$lib/functions/utils/execRequest/execRequest';
 
 export const load = (async ({ fetch }) => {
-	// Instance Friends Api
-	const api = new FriendsApi(fetch);
-	// Get Friends
-	const friends = await api.getFriends();
 
-	// Return data
+	const api = new FriendsApi(fetch);
+	const friends = await executeOrThrow(api.getFriends());
+
 	return {
-		friends
+		friends: friends.data,
 	};
+
 }) satisfies PageServerLoad;
 
 export const actions: Actions = {
 	findUserByEmail: async ({ request, fetch }) => {
-		// Get form data
 		const data = await request.formData();
 		const email = data.get('email') as string;
 
-		// Must have invitationId and accept
 		const emailSchema = yup.object().shape({
 			email: yup.string().email('Email invalide*').required('Email requis*')
 		});
 
 		const errors: { error?: string; email?: string } = {};
-		// ? Validation
 		try {
-			// Validation schema
 			await emailSchema.validate({ email }, { abortEarly: false });
 		} catch (error) {
-			// - Catch Errors
-			// If Error in ValidationError
 			if (error instanceof yup.ValidationError) {
-				// Check what error it is and return this in errors instance
 				error.inner.forEach((err) => {
 					if (err.path === 'email') {
 						errors.email = err.message;
 					}
 				});
 
-				// Return errors
 				return { status: 400, errors };
 			} else {
-				// Else Throw custom Error
 				return { status: 400, errors: 'Une erreur est survenue' };
 			}
 		}
 
-		// Instance Friends Api
 		const api = new UserApi(fetch);
 
-		// Get User by email
-		const user = await api.getUserByEmail(email);
-		if (!user) {
+		const res = await api.getUserByEmail(email);
+
+		if ("error" in res) {
+			errors.error = res.error.error;
+			return { status: 400, errors };
+		}
+
+		if (!res.data) {
 			errors.error = "Aucun utilisateur n'a été trouvé";
 			return { status: 400, errors };
 		}
 
-		if (user.error) {
-			// If an error occurs
-			errors.error = user.error;
-			return { status: 400, errors };
-		}
-
-		// Return data
 		return {
-			user
+			user: res.data,
 		};
 	},
 
@@ -107,7 +94,7 @@ export const actions: Actions = {
 		const res = await api.sendFriendsInvitation(id);
 
 		if ('error' in res) {
-			errors.error = res.error;
+			errors.error = res.error.error;
 			return { status: 400, errors };
 		}
 
