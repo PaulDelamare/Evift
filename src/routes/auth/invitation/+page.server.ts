ac2462c34a44c91460f482d829ddf41c36793f8f
PaulDelamare <paulodela2006@gmail.com>
2025-07-02 12:25:14 +0200

    🔧 Améliore la gestion des erreurs dans les actions d'invitation en utilisant executeOrThrow et en corrigeant l'accès à res.error.

--- src/routes/auth/invitation/+page.server.ts
+++ src/routes/auth/invitation/+page.server.ts
@@ -1,21 +1,20 @@
-// ! IMPORTANT
 import InvitationApi from '$lib/server/invitation.server';
 import type { Actions } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import * as yup from 'yup';
 import { convertStringToBoolean } from '$lib/functions/utils/transformStringToBoolean/transformStringToBoolean';
 import { env } from '$env/dynamic/private';
+import { executeOrThrow } from '$lib/functions/utils/execRequest/execRequest';
 
 export const load = (async ({ fetch }) => {
-	// Instance Invitation Api
+
 	const notifApi = new InvitationApi(fetch);
-	// Get Notification number
-	const invitations = await notifApi.getInvitations();
-	const invitationsEvent = await notifApi.getEventInvitation();
+
+	const invitations = await executeOrThrow(notifApi.getInvitations());
+	const invitationsEvent = await executeOrThrow(notifApi.getEventInvitation());
 
 	const imgUrl = env.API_URL;
 
-	// Return data
 	return {
 		invitations: invitations.data,
 		invitationsEvent: invitationsEvent.data,
@@ -23,14 +22,11 @@ export const load = (async ({ fetch }) => {
 	};
 }) satisfies PageServerLoad;
 
-// ! SCHEMA
-// Must have invitationId and accept
 const schema = yup.object().shape({
 	invitationId: yup.string().uuid().required(),
 	accept: yup.boolean().required()
 });
 
-// Errors model
 interface Errors {
 	error?: string;
 }
@@ -44,23 +40,16 @@ export const actions: Actions = {
 	 * @return Object containing success status, removed ID, and accept status.
 	 */
 	invitation: async ({ request, fetch }) => {
-		// Get form data
 		const data = await request.formData();
 
-		// Get form data values
 		const invitationId = data.get('invitationId') as string;
 		let accept = data.get('accept') as string | boolean;
 		const errors: Errors = {};
 
-		// ? Validation
 		try {
-			// Validation schema
 			await schema.validate({ invitationId, accept }, { abortEarly: false });
 		} catch (error) {
-			// - Catch Errors
-			// If Error in ValidationError
 			if (error instanceof yup.ValidationError) {
-				// Check what error it is and return this in errors instance
 				error.inner.forEach((err) => {
 					if (err.path === 'invitationId') {
 						errors.error = err.message;
@@ -71,29 +60,21 @@ export const actions: Actions = {
 					}
 				});
 
-				// Return errors
 				return { status: 400, errors };
 			} else {
-				// Else Throw custom Error
 				return { status: 400, errors: 'Une erreur est survenue' };
 			}
 		}
 
-		// Convert string to boolean
 		accept = convertStringToBoolean(accept as string);
 
-		// Instance Invitation in api
 		const api = new InvitationApi(fetch);
-		// Call responseInvitation
 		const res = await api.responseInvitation(invitationId, accept);
 
-		// if an error occurs
 		if ('error' in res) {
-			// Return error
-			errors.error = res?.error;
+			errors.error = res.error.error;
 			return { status: 400, errors };
 		}
-		// Else return success
 		return {
 			successFriend: true,
 			idRemove: invitationId,
@@ -102,21 +83,15 @@ export const actions: Actions = {
 	},
 
 	acceptOrRefuseEvent: async ({ request, fetch }) => {
-		// Get form data
 		const data = await request.formData();
 		const invitationId = data.get('eventId') as string;
 		let accept = data.get('accept') as string | boolean;
 		const errors: Errors = {};
 
-		// ? Validation
 		try {
-			// Validation schema
 			await schema.validate({ invitationId, accept }, { abortEarly: false });
 		} catch (error) {
-			// - Catch Errors
-			// If Error in ValidationError
 			if (error instanceof yup.ValidationError) {
-				// Check what error it is and return this in errors instance
 				error.inner.forEach((err) => {
 					if (err.path === 'invitationId') {
 						errors.error = err.message;
@@ -127,29 +102,22 @@ export const actions: Actions = {
 					}
 				});
 
-				// Return errors
 				return { status: 400, errors };
 			} else {
-				// Else Throw custom Error
 				return { status: 400, errors: 'Une erreur est survenue' };
 			}
 		}
 
-		// Convert string to boolean
 		accept = convertStringToBoolean(accept as string);
 
-		// Instance Invitation in api
 		const api = new InvitationApi(fetch);
-		// Call responseInvitation
 		const res = await api.responseEventInvitation(invitationId, accept);
 
-		// if an error occurs
 		if ('error' in res) {
-			// Return error
-			errors.error = res?.error;
+			errors.error = res?.error.error;
 			return { status: 400, errors };
 		}
-		// Else return success
+
 		return {
 			successEvent: true,
 			acceptEvent: accept,
