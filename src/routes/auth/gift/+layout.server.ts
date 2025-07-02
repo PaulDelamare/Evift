3af4ffaec9d6d920febc68fce26d835801ad5708
PaulDelamare <paulodela2006@gmail.com>
2025-07-02 12:25:07 +0200

    🔧 Améliore la gestion des erreurs dans la méthode load en utilisant executeOrThrow pour l'appel à findAll.

--- src/routes/auth/gift/+layout.server.ts
+++ src/routes/auth/gift/+layout.server.ts
@@ -1,9 +1,11 @@
+import { executeOrThrow } from '$lib/functions/utils/execRequest/execRequest';
 import GiftApi from '$lib/server/gift.server';
 import type { LayoutServerLoad } from './$types';
 
 export const load = (async ({ fetch }) => {
+
 	const api = new GiftApi(fetch);
-	const lists = await api.findAll();
+	const lists = await executeOrThrow(api.findAll())
 
 	return {
 		lists
