1c607ae12febec0a5b9b53f779ec1c70e055967e
PaulDelamare <paulodela2006@gmail.com>
2025-07-02 12:24:40 +0200

    🔧 Améliore la méthode load dans +page.server.ts en ajoutant une gestion des erreurs lors de l'appel à getEvent et en clarifiant la structure des données retournées.

--- src/routes/auth/event/+page.server.ts
+++ src/routes/auth/event/+page.server.ts
@@ -2,17 +2,17 @@ import type { Actions } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import EventApi from '$lib/server/event.server';
 import { env } from '$env/dynamic/private';
+import { executeOrThrow } from '$lib/functions/utils/execRequest/execRequest';
 
 export const load = (async ({ fetch }) => {
 
 	const api = new EventApi(fetch);
-	const allEvent = await api.getEvent();
-	
+	const allEvent = await executeOrThrow(api.getEvent());
 
 	const imgUrl = env.API_URL;
 
 	return {
-		allEvent,
+		allEvent: allEvent.data,
 		imgUrl
 	};
 }) satisfies PageServerLoad;
