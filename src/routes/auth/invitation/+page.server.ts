82c694fba69854b1eaa993576cd7ded471a907f7
PaulDelamare <paulodela2006@gmail.com>
2025-07-12 15:18:56 +0200

    🔧 Refactor la gestion des invitations : remplacement de Yup par Zod pour la validation des formulaires et simplification des actions de réponse aux invitations.

--- src/routes/auth/invitation/+page.server.ts
+++ src/routes/auth/invitation/+page.server.ts
@@ -1,10 +1,11 @@
 import InvitationApi from '$lib/server/invitation.server';
 import type { Actions } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
-import * as yup from 'yup';
-import { convertStringToBoolean } from '$lib/functions/utils/transformStringToBoolean/transformStringToBoolean';
 import { env } from '$env/dynamic/private';
 import { executeOrThrow } from '$lib/functions/utils/execRequest/execRequest';
+import { responseInvitationSchema } from '$lib/validationSchema/invitation.schema';
+import { message, superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
 
 export const load = (async ({ fetch }) => {
 
@@ -15,113 +16,73 @@ export const load = (async ({ fetch }) => {
 
 	const imgUrl = env.API_URL;
 
+	const formInvitationUser = await superValidate(zod(responseInvitationSchema));
+
 	return {
 		invitations: invitations.data,
 		invitationsEvent: invitationsEvent.data,
-		imgUrl
+		imgUrl,
+		formInvitationUser
 	};
 }) satisfies PageServerLoad;
 
-const schema = yup.object().shape({
-	invitationId: yup.string().uuid().required(),
-	accept: yup.boolean().required()
-});
-
-interface Errors {
-	error?: string;
-}
-
+/**
+ * SvelteKit form actions for handling invitation responses.
+ */
 export const actions: Actions = {
 	/**
-	 * Accept or reject an invitation.
-	 *
-	 * @param request - The request object for form data retrieval.
-	 * @param fetch - The fetch function for API calls.
-	 * @return Object containing success status, removed ID, and accept status.
+	 * Handles user response to a standard invitation.
+	 * @param event - The request event containing form data.
+	 * @returns A form message indicating success or error.
 	 */
-	invitation: async ({ request, fetch }) => {
-		const data = await request.formData();
+	invitation: async (event) => {
+		const form = await superValidate(event, zod(responseInvitationSchema));
 
-		const invitationId = data.get('invitationId') as string;
-		let accept = data.get('accept') as string | boolean;
-		const errors: Errors = {};
-
-		try {
-			await schema.validate({ invitationId, accept }, { abortEarly: false });
-		} catch (error) {
-			if (error instanceof yup.ValidationError) {
-				error.inner.forEach((err) => {
-					if (err.path === 'invitationId') {
-						errors.error = err.message;
-					}
-
-					if (err.path === 'accept') {
-						errors.error = err.message;
-					}
-				});
-
-				return { status: 400, errors };
-			} else {
-				return { status: 400, errors: 'Une erreur est survenue' };
-			}
+		if (!form.valid) {
+			return message(form, { error: 'Veuillez remplir tous les champs' });
 		}
 
-		accept = convertStringToBoolean(accept as string);
-
-		const api = new InvitationApi(fetch);
-		const res = await api.responseInvitation(invitationId, accept);
+		const api = new InvitationApi(event.fetch);
+		const res = await api.responseInvitation(form.data);
 
 		if ('error' in res) {
-			errors.error = res.error.error;
-			return { status: 400, errors };
+			return message(form, { error: res.error.error ?? 'Une erreur est survenue' });
 		}
-		return {
-			successFriend: true,
-			idRemove: invitationId,
-			acceptFriend: accept
-		};
+
+		return message(form, {
+			success: true,
+			message: form.data.response
+				? "Invitation acceptée avec succès."
+				: "Invitation refusée avec succès."
+		});
 	},
 
-	acceptOrRefuseEvent: async ({ request, fetch }) => {
-		const data = await request.formData();
-		const invitationId = data.get('eventId') as string;
-		let accept = data.get('accept') as string | boolean;
-		const errors: Errors = {};
+	/**
+	 * Handles user response to an event invitation.
+	 * @param event - The request event containing form data.
+	 * @returns A form message indicating success or error.
+	 */
+	acceptOrRefuseEvent: async (event) => {
+		const form = await superValidate(event, zod(responseInvitationSchema));
 
-		try {
-			await schema.validate({ invitationId, accept }, { abortEarly: false });
-		} catch (error) {
-			if (error instanceof yup.ValidationError) {
-				error.inner.forEach((err) => {
-					if (err.path === 'invitationId') {
-						errors.error = err.message;
-					}
-
-					if (err.path === 'accept') {
-						errors.error = err.message;
-					}
-				});
-
-				return { status: 400, errors };
-			} else {
-				return { status: 400, errors: 'Une erreur est survenue' };
-			}
+		if (!form.valid) {
+			return message(form, { error: 'Veuillez remplir tous les champs' });
 		}
 
-		accept = convertStringToBoolean(accept as string);
-
-		const api = new InvitationApi(fetch);
-		const res = await api.responseEventInvitation(invitationId, accept);
+		const api = new InvitationApi(event.fetch);
+		const res = await api.responseEventInvitation(form.data);
 
 		if ('error' in res) {
-			errors.error = res?.error.error;
-			return { status: 400, errors };
+			return message(form, {
+				error: res.error.error ?? 'Une erreur est survenue'
+			});
 		}
 
-		return {
-			successEvent: true,
-			acceptEvent: accept,
-			idRemoveEvent: invitationId
-		};
+		return message(form, {
+			success: true,
+			message: form.data.response
+				? "Invitation à l'événement acceptée avec succès."
+				: "Invitation à l'événement refusée avec succès."
+		});
 	}
 };
