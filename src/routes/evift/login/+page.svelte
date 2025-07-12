16a4c192a55f7946383b540ca976c911b31e26dc
PaulDelamare <paulodela2006@gmail.com>
2025-07-12 15:19:23 +0200

    🔧 Refactor la page de connexion : suppression de l'importation inutile de ActionData, simplification des commentaires et nettoyage du code pour une meilleure lisibilité.

--- src/routes/evift/login/+page.svelte
+++ src/routes/evift/login/+page.svelte
@@ -1,22 +1,16 @@
 <script lang="ts">
-	// Import components
 	import Layer from '$lib/components/public/loginRegister/layer/Layer.svelte';
 	import Login from '$lib/components/public/loginRegister/login/Login.svelte';
 	import Register from '$lib/components/public/loginRegister/register/Register.svelte';
 	import PageLayout from '$lib/components/structure/PageLayout.svelte';
-	import type { ActionData, PageData } from './$types';
+	import type { PageData } from './$types';
 
 	export let data: PageData;
-	export let form: ActionData;
 	const PUBLIC_CAPTCHA_KEY = data.captchaKey!;
 
-	// Variables
-	// Login variable for know if user is Activated
 	let loginActivated = true;
-	// Email variable
 	let email = '';
 
-	// Screen width
 	let innerWidth = 0;
 
 	// Functions
@@ -58,39 +52,32 @@
 	 */
 	const scrollToTop = (node: HTMLElement) => {
 		if (node) {
-			// Scroll to the element with smooth behavior.
 			node.scrollIntoView({ behavior: 'smooth' });
 		}
 	};
 </script>
 
-<!-- For know the screen width -->
 <svelte:window bind:innerWidth />
 
-<!-- Page Layout for Display Element -->
 <PageLayout padding="py-12 flex flex-col justify-center">
 	<!-- Wrap container -->
 	<section class="wrap px-8 mobile-large:px-4">
-		<!-- Login Register comoponen -->
 		<div
 			class="tablet:overflow-visible tablet:shadow-none tablet:gap-12 tablet:!flex-col rounded-xl flex overflow-hidden shadow-[41px_41px_82px_0_rgba(190,190,190,1),-41px_-41px_82px_0_rgba(255,255,255,1)] custom-transition min-h-[667px] relative {!loginActivated
 				? 'justify-end'
 				: ''}"
 		>
-			<!-- Add Login and Register component -->
-			<!-- Login with Form -->
-			<Login {PUBLIC_CAPTCHA_KEY} {form} {loginActivated} {email} {handleEmailChange} />
-			<!-- Register with Form -->
+
+			<Login {PUBLIC_CAPTCHA_KEY} {loginActivated} {email} {handleEmailChange} />
+
 			<Register
 				{PUBLIC_CAPTCHA_KEY}
-				{form}
 				{loginActivated}
 				{email}
 				{handleEmailChange}
 				{handleActivate}
 			/>
 
-			<!-- Add Layer component for change into login or register -->
 			<Layer {loginActivated} {handleActivate} />
 		</div>
 	</section>
