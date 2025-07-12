8aa9f85cd8b47402518932da2e800b3454ca0e44
PaulDelamare <paulodela2006@gmail.com>
2025-07-12 15:17:18 +0200

    🔧 Refactor la gestion du changement de rôle des participants : remplacement de Yup par Zod pour la validation des formulaires et simplification de la logique de traitement des erreurs.

--- src/routes/auth/event/event-[id_event]/+page.server.ts
+++ src/routes/auth/event/event-[id_event]/+page.server.ts
@@ -1,62 +1,34 @@
 import type { Actions } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
-import * as yup from 'yup';
 import EventApi from '$lib/server/event.server';
+import { zod } from 'sveltekit-superforms/adapters';
+import { message, superValidate } from 'sveltekit-superforms';
+import { changeRoleParticipantSchema } from '$lib/validationSchema/participant.schema';
 
 export const load = (async () => {
-	return {};
+
+	const form = await superValidate(zod(changeRoleParticipantSchema));
+	return {
+		form
+	};
 }) satisfies PageServerLoad;
 
 export const actions: Actions = {
-	changeRoleParticipant: async ({ request, fetch }) => {
-		// Get form data
-		const data = await request.formData();
+	changeRoleParticipant: async (event) => {
 
-		// Get form data values
-		const id_role = data.get('id_role') as string;
-		const id_user = data.get('id_user') as string;
-		const id_event = data.get('id_event') as string;
+		const form = await superValidate(event, zod(changeRoleParticipantSchema));
 
-		const errors: { error?: string } = {};
-
-		// Must have invitationId and accept
-		const schema = yup.object().shape({
-			id_role: yup.string().uuid().required(),
-			id_user: yup.string().uuid().required(),
-			id_event: yup.string().uuid().required()
-		});
-
-		// ? Validation
-		try {
-			// Validation schema
-			await schema.validate({ id_role, id_user, id_event }, { abortEarly: false });
-		} catch (error) {
-			// - Catch Errors
-			// If Error in ValidationError
-			if (error instanceof yup.ValidationError) {
-				// Check what error it is and return this in errors instance
-				error.inner.forEach((err) => {
-					errors.error = err.message;
-				});
-
-				// Return errors
-				return { status: 400, errors };
-			} else {
-				// Else Throw custom Error
-				return { status: 400, errors: 'Une erreur est survenue' };
-			}
+		if (!form.valid) {
+			return message(form, { error: 'Veuillez remplir tous les champs' });
 		}
 
-		// Instance Invitation in api
-		const api = new EventApi(fetch);
-
-		// Call responseInvitation
-		const res = await api.changeRoleParticipant(id_event, id_user, id_role);
+		const api = new EventApi(event.fetch);
+		const res = await api.changeRoleParticipant(form.data);
 
 		if ('error' in res) {
-			return { status: 400, errors: res.error.error };
+			return message(form, { status: 400, error: res.error.error });
 		}
 
-		return { status: 200, success: true };
+		return message(form, { success: true, message: 'Rôle modifié avec succès' });
 	}
 };
