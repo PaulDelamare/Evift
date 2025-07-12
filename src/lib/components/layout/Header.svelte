33991058d22cabaf5c08ed9982e86c1c07b7e073
PaulDelamare <paulodela2006@gmail.com>
2025-07-12 15:07:22 +0200

    🔧 Refactor le composant Header en corrigeant l'importation de 'page' et en supprimant les commentaires inutiles pour améliorer la lisibilité.

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -1,23 +1,16 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-
-	// Imports
+	import { page } from '$app/state';
 	import Burger from '../extra/header/Burger.svelte';
 	import Logo from '../svg/Logo.svelte';
 	import Navigation from './Navigation.svelte';
 
-	$: user = $page.data.user;
+	$: user = page.data.user;
 </script>
 
-<!-- Bloc for Header -->
 <div class="wrap py-2 px-8 between items-end relative">
-	<!-- Display Logo -->
 	<Logo gradient text link h1 />
 
-	<!-- TODO Add condition for Loged Nav -->
-	<!-- For Desktop Display Navigation -->
 	<Navigation navClass={!user ? 'tablet:hidden' : 'mini-desk:hidden'} ulClass="gap-12 pb-2" hover />
 
-	<!-- For Responsive Display Burger Menu -->
 	<Burger divClass="hidden {!user ? 'tablet:block' : 'mini-desk:block'}" />
 </div>
