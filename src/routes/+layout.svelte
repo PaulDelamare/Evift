968f1b438163b6bccd314b6b4be369dbf8111408
PaulDelamare <paulodela2006@gmail.com>
2025-07-12 15:16:38 +0200

    🔧 Nettoyage des commentaires dans le fichier +layout.svelte pour améliorer la lisibilité du code.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	// Imports
 	import Footer from '$lib/components/layout/Footer.svelte';
 	import Header from '$lib/components/layout/Header.svelte';
 	import CustomAppShell from '$lib/components/structure/CustomAppShell.svelte';
@@ -19,17 +18,13 @@
 
 <svelte:window on:popstate={handlePopstate} />
 
-<!-- Create Custom APP Shell for Element Position -->
 <CustomAppShell
 	headerSticky
 	headerClass="bg-surface-500 shadow-header"
 	footerClass="bg-secondaryGradient"
 >
-	<!-- Add Header -->
 	<Header slot="header" />
-	<!-- Define Slot into Header and Footer -->
 	<slot />
-	<!-- Add Footer -->
 	<Footer slot="footer" />
 </CustomAppShell>
 
