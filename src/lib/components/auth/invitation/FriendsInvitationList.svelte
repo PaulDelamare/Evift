45dbc2df6fb478572922ae073266dd63bcea6da6
PaulDelamare <paulodela2006@gmail.com>
2025-07-12 15:05:50 +0200

    🔧 Nettoie le composant FriendsInvitationList en supprimant le code inutilisé et en simplifiant la logique d'affichage des invitations.

--- src/lib/components/auth/invitation/FriendsInvitationList.svelte
+++ src/lib/components/auth/invitation/FriendsInvitationList.svelte
@@ -1,26 +1,14 @@
 <script lang="ts">
-	// ! Import
 	import type { GetUser } from '$lib/models/user.model';
-	import type { ActionData } from '../../../../routes/auth/invitation/$types';
 	import User from './User.svelte';
 
-	// Get Data
 	export let dataArray: GetUser[] = [];
-	export let form: ActionData = null;
 	export let button = true;
 	export let sizeImg = 'size-32';
 	export let shadow =
 		'shadow-[31px_31px_82px_0_rgba(190,190,190,1),-31px_-31px_82px_0_rgba(255,255,255,1)]';
 
-	// If form is succes (accept or refuse invitation)
-	$: if (form?.successFriend) {
-		// Remove invitation from list
-		dataArray = dataArray.filter((invitation) => invitation.id !== form.idRemove);
-	}
-
-	// If it's friends page
 	export let friends = false;
-	// When there is no data
 	export let nothingMessage = "Vous ne possédez pour l'instant aucun ami.";
 </script>
 
@@ -30,14 +18,10 @@ The component get invitation from the server
 @returnServerSide {Invitation[]} dataArray - The list of invitation
 @params {ActionData} form - The form
  -->
-<!-- Start of component -->
 <ul class="column gap-12 {friends ? 'gap-4' : ''}">
-	{#if dataArray.length > 0}
-		<!-- For each invitation -->
-		{#each dataArray as data}
-			<User {friends} {data} {button} {shadow} {sizeImg} />
-		{/each}
+	{#each dataArray as data}
+		<User {friends} {data} {button} {shadow} {sizeImg} />
 	{:else}
 		<h4>{nothingMessage}</h4>
-	{/if}
+	{/each}
 </ul>
