50a917162ee890063ca9772c9c55285ffe8ebca7
PaulDelamare <paulodela2006@gmail.com>
2025-08-14 17:25:24 +0200

    🔧 Réorganisation de la configuration Vite pour inclure des options de build, d'optimisation des dépendances et de gestion SSR

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,20 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), purgeCss()]
+    plugins: [sveltekit(), purgeCss()],
+    build: {
+        target: ['es2022'], // Cible plus récente pour supporter top-level await
+        minify: true
+    },
+    optimizeDeps: {
+        // Exclure les dépendances qui pourraient causer des problèmes
+        exclude: [
+            'driver.js', // car vous l'utilisez et il pourrait causer des problèmes
+            '@vinejs/vine' // même si vous ne l'utilisez pas directement
+        ]
+    },
+    // Ajout pour gérer les modules Node.js qui pourraient être importés
+    ssr: {
+        noExternal: ['driver.js'] // Garder driver.js dans le bundle SSR
+    }
 });
